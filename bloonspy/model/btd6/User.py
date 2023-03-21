eba959cd7c5f89d41bec81c8fd50ced4b5ed5e76
SartoRiccardo <rikki.sarto@gmail.com>
2023-03-21 15:35:45 +0100

    User model + tests

--- bloonspy/model/btd6/User.py
+++ bloonspy/model/btd6/User.py
@@ -1,5 +1,9 @@
 from dataclasses import dataclass, field
-from typing import Dict, List
+import requests
+from typing import Dict, List, Any
+from ...exceptions import NotFound
+from ...utils.decorators import fetch_property
+from ...utils.dictionaries import rename_keys
 from ..Asset import Asset
 from .Tower import Tower
 from .Medals import Medals, MapMedals, CTGlobalMedals, CTLocalMedals
@@ -7,60 +11,257 @@ from .Medals import Medals, MapMedals, CTGlobalMedals, CTLocalMedals
 
 @dataclass(kw_only=True)
 class BloonsPoppedStats:
-    total: int
-    total_coop: int
-    camos: int
-    regrows: int
-    purples: int
-    leads: int
-    ceramics: int
-    moabs: int
-    bfbs: int
-    zomgs: int
-    bads: int
-    golden: int
+    total: int = field(default=0)
+    total_coop: int = field(default=0)
+    camos: int = field(default=0)
+    regrows: int = field(default=0)
+    purples: int = field(default=0)
+    leads: int = field(default=0)
+    ceramics: int = field(default=0)
+    moabs: int = field(default=0)
+    bfbs: int = field(default=0)
+    zomgs: int = field(default=0)
+    bads: int = field(default=0)
+    golden: int = field(default=0)
 
 
 @dataclass(kw_only=True)
 class GameplayStats:
-    most_experienced_monkey: Tower
-    cash_earned: int
-    challenges_completed: int
-    collection_chests_opened: int
-    coop_cash_given: int
-    daily_rewards: int
-    game_count: int
-    games_won: int
-    highest_round: int
-    highest_round_chimps: int
-    highest_round_deflation: int
-    insta_monkey_collection: int
-    monkey_teams_wins: int
-    powers_used: int
-    total_odysseys_completed: int
-    total_odyssey_stars: int
-    total_trophies_earned: int
-    necro_bloons_reanimated: int
-    bloons_leaked: int
-    bloons_popped: BloonsPoppedStats
+    most_experienced_monkey: Tower = field(default=Tower.DART_MONKEY)
+    cash_earned: int = field(default=0)
+    challenges_completed: int = field(default=0)
+    collection_chests_opened: int = field(default=0)
+    coop_cash_given: int = field(default=0)
+    daily_rewards: int = field(default=0)
+    game_count: int = field(default=0)
+    games_won: int = field(default=0)
+    highest_round: int = field(default=0)
+    highest_round_chimps: int = field(default=0)
+    highest_round_deflation: int = field(default=0)
+    insta_monkey_collection: int = field(default=0)
+    monkey_teams_wins: int = field(default=0)
+    powers_used: int = field(default=0)
+    total_odysseys_completed: int = field(default=0)
+    total_odyssey_stars: int = field(default=0)
+    total_trophies_earned: int = field(default=0)
+    necro_bloons_reanimated: int = field(default=0)
+    bloons_leaked: int = field(default=0)
+    bloons_popped: BloonsPoppedStats = field(default_factory=BloonsPoppedStats)
 
 
-@dataclass(kw_only=True)
 class User:
-    id: str
-    name: str
-    rank: int
-    veteran_rank: int
-    achivements: int
-    followers: int
-    avatar: Asset
-    banner: Asset
-    single_player_medals: MapMedals
-    coop_medals: MapMedals
-    boss_normal_medals: Medals
-    boss_elite_medals: Medals
-    race_medals: Medals
-    ct_local_medals: CTLocalMedals
-    ct_global_medals: CTGlobalMedals
-    stats: GameplayStats
-    heroes_placed: Dict[Tower, int]
+    endpoint = "https://data.ninjakiwi.com/btd6/users/{}"
+
+    def __init__(self, user_id: str, eager: bool = False):
+        self._id = user_id
+        self._data = {}
+        self._loaded = False
+        if eager:
+            self._load_user()
+
+    def _load_user(self, only_if_unloaded: bool = True) -> None:
+        if self._loaded and only_if_unloaded:
+            return
+
+        resp = requests.get(self.endpoint.format(self._id))
+        if resp.status_code != 200:
+            return
+
+        data = resp.json()
+        if not data["success"]:
+            self._handle_exceptions(data["error"])
+
+        self._parse_json(data["body"])
+
+    @staticmethod
+    def _handle_exceptions(error_msg: str) -> None:
+        if error_msg == "Invalid user ID / Player Does not play this game":
+            raise NotFound(error_msg)
+        raise Exception(error_msg)
+
+    def _parse_json(self, raw_user: Dict[str, Any]) -> None:
+        self._loaded = False
+
+        self._data = {}
+        copy_keys = ["displayName", "rank", "veteranRank", "achievements", "followers"]
+        for key in copy_keys:
+            self._data[key] = raw_user[key]
+
+        self._data["avatar"] = Asset(raw_user["avatar"], raw_user["avatarURL"])
+        self._data["banner"] = Asset(raw_user["banner"], raw_user["bannerURL"])
+
+        map_medal_keys = [
+            ("CHIMPS-BLACK", "chimps_black"), ("Clicks", "chimps_red"), ("Easy", "easy"), ("Medium", "medium"),
+            ("Hard", "hard"), ("PrimaryOnly", "primary_only"), ("Deflation", "deflation"),
+            ("MilitaryOnly", "military_only"), ("Apopalypse", "apopalypse"), ("Reverse", "reverse"),
+            ("MagicOnly", "magic_only"), ("HalfCash", "half_cash"), ("DoubleMoabHealth", "double_hp_moabs"),
+            ("AlternateBloonsRounds", "alternate_bloons_rounds"), ("Impoppable", "impoppable")
+        ]
+        self._data["single_player_medals"] = MapMedals(
+            **rename_keys(raw_user["_medalsSinglePlayer"], map_medal_keys)
+        )
+        self._data["coop_medals"] = MapMedals(
+            **rename_keys(raw_user["_medalsMultiplayer"], map_medal_keys)
+        )
+
+        event_medal_keys = [
+            ("BlackDiamond", "first"), ("RedDiamond", "second"), ("Diamond", "third"), ("GoldDiamond", "top_50"),
+            ("DoubleGold", "top_1_percent"), ("GoldSilver", "top_10_percent"), ("DoubleSilver", "top_25_percent"),
+            ("Silver", "top_50_percent"), ("Bronze", "top_75_percent")
+        ]
+        self._data["boss_normal_medals"] = Medals(
+            **rename_keys(raw_user["_medalsBoss"], event_medal_keys)
+        )
+        self._data["boss_elite_medals"] = Medals(
+            **rename_keys(raw_user["_medalsBossElite"], event_medal_keys)
+        )
+        # self._data["race_medals"] = Medals(
+        #     **rename_keys(raw_user["_medals"], event_medal_keys)
+        # )
+
+        ct_local_medal_keys = [
+            ("BlackDiamond", "first"), ("RedDiamond", "second"), ("Diamond", "third"), ("GoldDiamond", "top_10"),
+            ("DoubleGold", "top_20"), ("Silver", "top_40"), ("Bronze", "top_60")
+        ]
+        ct_global_medal_keys = [
+            ("Diamond", "top_25"), ("GoldDiamond", "top_100"), ("DoubleGold", "top_1_percent"),
+            ("GoldSilver", "top_10_percent"), ("DoubleSilver", "top_25_percent"), ("Silver", "top_50_percent"),
+            ("Bronze", "top_75_percent")
+        ]
+        self._data["ct_local_medals"] = CTLocalMedals(
+            **rename_keys(raw_user["_medalsCTLocal"], ct_local_medal_keys)
+        )
+        self._data["ct_global_medals"] = CTGlobalMedals(
+            **rename_keys(raw_user["_medalsCTLocal"], ct_global_medal_keys)
+        )
+
+        stats_keys = [
+            ("gameplay.cashEarned", "cash_earned"),
+            ("gameplay.challengesCompleted", "challenges_completed"),
+            ("gameplay.collectionChestsOpened", "collection_chests_opened"),
+            ("gameplay.coopCashGiven", "coop_cash_given"),
+            ("gameplay.dailyRewards", "daily_rewards"),
+            ("gameplay.gameCount", "game_count"),
+            ("gameplay.gamesWon", "games_won"),
+            ("gameplay.highestRound", "highest_round"),
+            ("gameplay.highestRoundCHIMPS", "highest_round_chimps"),
+            ("gameplay.highestRoundDeflation", "highest_round_deflation"),
+            ("gameplay.instaMonkeyCollection", "insta_monkey_collection"),
+            ("gameplay.monkeyTeamsWins", "monkey_teams_wins"),
+            ("gameplay.powersUsed", "powers_used"),
+            ("gameplay.totalOdysseysCompleted", "total_odysseys_completed"),
+            ("gameplay.totalOdysseyStars", "total_odyssey_stars"),
+            ("gameplay.totalTrophiesEarned", "total_trophies_earned"),
+            ("bloonsPopped.necroBloonsReanimated", "necro_bloons_reanimated"),
+            ("bloonsPopped.bloonsLeaked", "bloons_leaked"),
+        ]
+        bloons_popped_keys = [
+            ("badsPopped", "bads"),
+            ("bfbsPopped", "bfbs"),
+            ("bloonsPopped", "total"),
+            ("camosPopped", "camos"),
+            ("ceramicsPopped", "ceramics"),
+            ("coopBloonsPopped", "total_coop"),
+            ("goldenBloonsPopped", "golden"),
+            ("leadsPopped", "leads"),
+            ("moabsPopped", "moabs"),
+            ("purplesPopped", "purples"),
+            ("regrowsPopped", "regrows"),
+            ("zomgsPopped", "zomgs"),
+        ]
+        self._data["stats"] = GameplayStats(
+            most_experienced_monkey=Tower.from_string(raw_user["mostExperiencedMonkey"]),
+            bloons_popped=BloonsPoppedStats(**rename_keys(raw_user["bloonsPopped"], bloons_popped_keys)),
+            **rename_keys(raw_user, stats_keys)
+        )
+
+        heroes_placed = {}
+        for hero in raw_user["heroesPlaced"].keys():
+            heroes_placed[Tower.from_string(hero)] = raw_user["heroesPlaced"][hero]
+        self._data["heroes_placed"] = heroes_placed
+
+        self._loaded = True
+
+    @property
+    def id(self) -> str:
+        return self._id
+
+    @property
+    @fetch_property(_load_user)
+    def name(self) -> str:
+        return self._data["displayName"]
+
+    @property
+    @fetch_property(_load_user)
+    def rank(self) -> int:
+        return self._data["rank"]
+
+    @property
+    @fetch_property(_load_user)
+    def veteran_rank(self) -> int:
+        return self._data["veteranRank"]
+
+    @property
+    @fetch_property(_load_user)
+    def achievements(self) -> int:
+        return self._data["achievements"]
+
+    @property
+    @fetch_property(_load_user)
+    def followers(self) -> int:
+        return self._data["followers"]
+
+    @property
+    @fetch_property(_load_user)
+    def avatar(self) -> Asset:
+        return self._data["avatar"]
+
+    @property
+    @fetch_property(_load_user)
+    def banner(self) -> Asset:
+        return self._data["banner"]
+
+    @property
+    @fetch_property(_load_user)
+    def single_player_medals(self) -> MapMedals:
+        return self._data["single_player_medals"]
+
+    @property
+    @fetch_property(_load_user)
+    def coop_medals(self) -> MapMedals:
+        return self._data["coop_medals"]
+
+    @property
+    @fetch_property(_load_user)
+    def boss_normal_medals(self) -> Medals:
+        return self._data["boss_normal_medals"]
+
+    @property
+    @fetch_property(_load_user)
+    def boss_elite_medals(self) -> Medals:
+        return self._data["boss_elite_medals"]
+
+    # @property
+    # @fetch_property(_load_user)
+    # def race_medals(self) -> Medals:
+    #     return self._data["race_medals"]
+
+    @property
+    @fetch_property(_load_user)
+    def ct_local_medals(self) -> CTLocalMedals:
+        return self._data["ct_local_medals"]
+
+    @property
+    @fetch_property(_load_user)
+    def ct_global_medals(self) -> CTGlobalMedals:
+        return self._data["ct_global_medals"]
+
+    @property
+    @fetch_property(_load_user)
+    def stats(self) -> GameplayStats:
+        return self._data["stats"]
+
+    @property
+    @fetch_property(_load_user)
+    def heroes_placed(self) -> Dict[Tower, int]:
+        return self._data["heroes_placed"]
---
+++ tests/2_test_user.py
@@ -0,0 +1,147 @@
+import unittest
+from datetime import datetime
+import bloonspy
+from bloonspy import btd6
+
+
+class TestChallenge(unittest.TestCase):
+    def test_challenge(self) -> None:
+        """
+        Test that a challenge is loaded correctly.
+        """
+        user_id = "9cee138c8c94ffac1910864c0b73e577ca554ce8cb18db6c"
+        user = btd6.User(user_id)
+
+        expected_results = [
+            ("id", "9cee138c8c94ffac1910864c0b73e577ca554ce8cb18db6c"),
+            ("name", "Sarto"),
+            ("rank", 155),
+        ]
+        for attr_name, attr_expected_value in expected_results:
+            self.assertEqual(getattr(user, attr_name), attr_expected_value,
+                msg=f"Asserting User.{attr_name}")
+
+        # Greater or Equal cause these can increase in the future.
+        expected_results_ge = [
+            ("achievements", 124),
+            ("veteran_rank", 8),
+        ]
+        for attr_name, attr_expected_min in expected_results_ge:
+            self.assertGreaterEqual(getattr(user, attr_name), attr_expected_min,
+                msg=f"Asserting User.{attr_name}")
+
+        check_instance = [
+            ("followers", int),
+            ("avatar", btd6.Asset),
+            ("banner", btd6.Asset),
+            ("single_player_medals", btd6.MapMedals),
+            ("coop_medals", btd6.MapMedals),
+            ("boss_normal_medals", btd6.Medals),
+            ("boss_elite_medals", btd6.Medals),
+            # ("race_medals", btd6.Medals),
+            ("ct_local_medals", btd6.CTLocalMedals),
+            ("ct_global_medals", btd6.CTGlobalMedals),
+            ("stats", btd6.GameplayStats),
+        ]
+        for attr_name, attr_type in check_instance:
+            self.assertIsInstance(getattr(user, attr_name), attr_type,
+                msg=f"Assert if challenge.{attr_name} is {attr_type}")
+
+        # Check medals
+
+        expected_results_ge = [
+            ("chimps_black", 1), ("easy", 1), ("medium", 1), ("hard", 1),
+            ("primary_only", 1), ("deflation", 1), ("military_only", 1),
+            ("apopalypse", 1), ("reverse", 1), ("magic_only", 1), ("half_cash", 1),
+            ("double_hp_moabs", 1), ("alternate_bloons_rounds", 1), ("impoppable", 1),
+            ("chimps_red", 1),
+        ]
+        for attr_name, attr_expected_min in expected_results_ge:
+            self.assertGreaterEqual(getattr(user.single_player_medals, attr_name), attr_expected_min,
+                                    msg=f"Asserting User.single_player_medals.{attr_name}")
+            self.assertGreaterEqual(getattr(user.coop_medals, attr_name), attr_expected_min,
+                                    msg=f"Asserting User.coop_medals.{attr_name}")
+
+        check_instance = [
+            ("first", int), ("second", int), ("third", int), ("top_50", int), ("top_1_percent", int),
+            ("top_10_percent", int), ("top_25_percent", int), ("top_50_percent", int), ("top_75_percent", int)
+        ]
+        for attr_name, attr_type in check_instance:
+            self.assertIsInstance(getattr(user.boss_normal_medals, attr_name), attr_type,
+                                  msg=f"Asserting User.boss_normal_medals.{attr_name}")
+            self.assertIsInstance(getattr(user.boss_elite_medals, attr_name), attr_type,
+                                  msg=f"Asserting User.boss_elite_medals.{attr_name}")
+            # self.assertIsInstance(
+            #     getattr(user.race_medals, attr_name), attr_type,
+            #     msg=f"Asserting User.race_medals.{attr_name}"
+            # )
+
+        check_instance = [
+            ("first", int), ("second", int), ("third", int), ("top_10", int), ("top_20", int),
+            ("top_40", int), ("top_60", int)
+        ]
+        for attr_name, attr_type in check_instance:
+            self.assertIsInstance(getattr(user.ct_local_medals, attr_name), attr_type,
+                                  msg=f"Asserting User.ct_local_medals.{attr_name}")
+
+        check_instance = [
+            ("top_25", int), ("top_100", int), ("top_1_percent", int), ("top_10_percent", int), ("top_25_percent", int),
+            ("top_50_percent", int), ("top_75_percent", int)
+        ]
+        for attr_name, attr_type in check_instance:
+            self.assertIsInstance(getattr(user.ct_global_medals, attr_name), attr_type,
+                                  msg=f"Asserting User.ct_global_medals.{attr_name}")
+
+        # Gameplay stats
+
+        expected_results_ge = [
+            (btd6.Tower.BRICKELL, 93), (btd6.Tower.ADORA, 68), (btd6.Tower.BENJAMIN, 1189),
+            (btd6.Tower.ETIENNE, 108), (btd6.Tower.GERALDO, 151), (btd6.Tower.GWENDOLIN, 411),
+            (btd6.Tower.OBYN, 499), (btd6.Tower.PAT_FUSTY, 143), (btd6.Tower.PSI, 204),
+            (btd6.Tower.QUINCY, 125), (btd6.Tower.SAUDA, 534), (btd6.Tower.STRIKER_JONES, 59),
+            (btd6.Tower.EZILI, 257), (btd6.Tower.CHURCHILL, 63),
+        ]
+        for hero, times_placed in expected_results_ge:
+            self.assertIn(hero, user.heroes_placed.keys(),
+                          msg=f"Assert {hero.value} appears in User.heroes_placed")
+            self.assertGreaterEqual(times_placed, user.heroes_placed[hero],
+                                    msg=f"Assert correctness of User.heroes_placed[{hero.value}]")
+
+        self.assertIsInstance(user.stats.most_experienced_monkey, btd6.Tower,
+                              msg=f"Asserting User.stats.most_experienced_monkey")
+        expected_results_ge = [
+            ("cash_earned", 711700673), ("challenges_completed", 198), ("collection_chests_opened", 293),
+            ("coop_cash_given", 78006898), ("daily_rewards", 339), ("game_count", 9252), ("games_won", 2618),
+            ("highest_round", 276), ("highest_round_chimps", 127), ("highest_round_deflation", 100),
+            ("insta_monkey_collection", 796), ("monkey_teams_wins", 11), ("powers_used", 3811),
+            ("total_odysseys_completed", 26), ("total_odyssey_stars", 100), ("total_trophies_earned", 3288),
+            ("necro_bloons_reanimated", 476791), ("bloons_leaked", 557788),
+        ]
+        for attr_name, attr_expected_min in expected_results_ge:
+            self.assertGreaterEqual(getattr(user.stats, attr_name), attr_expected_min,
+                                    msg=f"Asserting User.stats.{attr_name}")
+
+        expected_results_ge = [
+            ("total", 455698668), ("total_coop", 26290553), ("camos", 31764834), ("regrows", 293382673),
+            ("purples", 1133115), ("leads", 13291249), ("ceramics", 210121378), ("moabs", 1324838), ("bfbs", 252424),
+            ("zomgs", 34974), ("bads", 3824), ("golden", 69),
+        ]
+        for attr_name, attr_expected_min in expected_results_ge:
+            self.assertGreaterEqual(getattr(user.stats.bloons_popped, attr_name), attr_expected_min,
+                                    msg=f"Asserting User.stats.bloons_popped.{attr_name}")
+
+    def test_user_fail(self) -> None:
+        """
+        Test that the correct exception is raised when attempting to fetch a nonexistant user.
+        """
+        user_id = "blatantlywronguserid"
+        correct_exception = False
+        try:
+            btd6.User(user_id, eager=True)
+        except btd6.NotFound:
+            correct_exception = True
+        self.assertTrue(correct_exception, msg="Wrong user IDs should raise bloonspy.exceptions.NotFound")
+
+
+if __name__ == '__main__':
+    unittest.main()
