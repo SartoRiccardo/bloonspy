cb34c3b952db815118e19571e0b4d61ce7516039
SartoRiccardo <rikki.sarto@gmail.com>
2023-04-06 15:50:43 +0200

    BossPlayer & RacePlayer score fix; Coop Boss leaderboards

--- tests/integration/test_boss_leaderboard.py
+++ tests/integration/test_boss_leaderboard.py
@@ -28,10 +28,27 @@ class TestBossLeaderboard(unittest.TestCase):
             self.assertIsInstance(getattr(some_player, attr_name), attr_type,
                                   msg=f"Assert if BossPlayer.{attr_name} is {attr_type}")
 
-    # def test_boss_leaderboard_coop(self) -> None:
-    #     """
-    #     Test fetching a boss' coop leaderboard.
-    #     """
+    def test_boss_leaderboard_coop(self) -> None:
+        """
+        Test fetching a boss' coop leaderboard.
+        """
+        bosses = Client.bosses()
+        boss = bosses[1]
+        boss_leaderboard_coop = boss.standard().leaderboard(pages=3, team_size=3)
+
+        check_instance = [
+            ("score", timedelta), ("submission_time", datetime), ("is_fully_loaded", bool)
+        ]
+        for i in range(len(boss_leaderboard_coop)):
+            team = boss_leaderboard_coop[i]
+            self.assertLessEqual(len(team.players), 3,
+                                 msg="Assert if BossPlayerTeam has the correct number of players.")
+            for attr_name, attr_type in check_instance:
+                self.assertIsInstance(getattr(team, attr_name), attr_type,
+                                      msg=f"Assert if BossPlayerTeam.{attr_name} is {attr_type}")
+                for player in team.players:
+                    self.assertIsInstance(player, btd6.BossPlayer,
+                                          msg="Assert if BossPlayerTeam.players is a tuple of BossPlayer.")
 
 
 if __name__ == '__main__':
