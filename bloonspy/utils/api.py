c314e41a896032a0991a344b89766877e6784d26
SartoRiccardo <rikki.sarto@gmail.com>
2023-04-11 20:29:10 +0200

    Rate limit handling

--- bloonspy/utils/api.py
+++ bloonspy/utils/api.py
@@ -1,34 +1,59 @@
 import requests
+import threading
+import time
+import random
 from typing import Dict, Any, List, Union
 import sys
 
 
 API_URL = "https://data.ninjakiwi.com"
 
+request_lock = None
+
+
+def lock_requests(lock_time: float) -> None:
+    global request_lock
+    time.sleep(lock_time)
+    request_lock = None
+
 
 def get(endpoint: str, params: Dict[str, Any] = None) -> Union[List[Dict[str, Any]], Dict[str, Any]]:
+    global request_lock
+
     if "unittest" in sys.modules.keys():
         print(f"GET {endpoint}, {params=}")
 
     if params is None:
         params = {}
 
-    resp = requests.get(API_URL + endpoint, params=params, headers={"User-Agent": "bloonspy Python Library"})
-    if resp.status_code >= 500:
-        if "unittest" in sys.modules.keys():
-            print(resp.content)
-            print(resp.headers)
-        raise Exception("Server error occurred")
-    if resp.status_code >= 400:
-        raise Exception("Bad request")
-    if "application/json" not in resp.headers.get("content-type").lower():
-        raise Exception("Content is not JSON")
+    while True:
+        while request_lock is not None:
+            request_lock.join()
 
-    data = resp.json()
-    if not data["success"]:
-        raise Exception(data["error"])
+        resp = requests.get(API_URL + endpoint, params=params, headers={"User-Agent": "bloonspy Python Library"})
+        if resp.status_code >= 500:
+            if "unittest" in sys.modules.keys():
+                print(resp.content)
+                print(resp.headers)
+            raise Exception("Server error occurred")
+        if resp.status_code >= 400:
+            if resp.status_code == 403 and "Retry-After" in resp.headers:
+                retry_after = int(resp.headers["Retry-After"]) + random.random()*3
+                if "unittest" in sys.modules.keys():
+                    print(f"Hit rate limit. Retry after {retry_after}s.")
+                request_lock = threading.Thread(target=lock_requests, args=(retry_after, ))
+                request_lock.start()
+                continue
 
-    return data["body"]
+            raise Exception("Bad request")
+        if "application/json" not in resp.headers.get("content-type").lower():
+            raise Exception("Content is not JSON")
+
+        data = resp.json()
+        if not data["success"]:
+            raise Exception(data["error"])
+
+        return data["body"]
 
 
 def get_lb_page(endpoint: str, page_num: int):
